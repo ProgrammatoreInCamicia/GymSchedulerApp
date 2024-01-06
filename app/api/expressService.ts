c651a80a0522cea099ec7a47a24903474a2ce435
Stefan Todirica <programmatoreincamicia@gmail.com>
2024-01-06 11:47:34 +0100

    changed config

--- app/api/expressService.ts
+++ app/api/expressService.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://192.168.1.58:3000/api/exercise'
+    baseURL: 'http://13.49.64.199:3000/api/exercise'
+    // baseURL: 'http://192.168.1.58:3000/api/exercise'
 });
